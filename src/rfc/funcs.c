6715495738a15e54e208a7f3dc93888b8623c7bc
HellFireInfernoStorm <dinura2003@gmail.com>
2024-07-06 12:05:30 +0530

    add G_md5 aux func and fix I aux func in funcs.c

--- src/rfc/funcs.c
+++ src/rfc/funcs.c
@@ -26,7 +26,12 @@ uint32_t H(uint32_t X, uint32_t Y, uint32_t Z)
 
 uint32_t I(uint32_t X, uint32_t Y, uint32_t Z)
 {
-	return X ^ (Y | (~Z));
+	return Y ^ (X | (~Z));
+}
+
+uint32_t G_md5(uint32_t X, uint32_t Y, uint32_t Z)
+{
+	return (X & Z) | (Y & (~Z));
 }
 
 uint32_t ROTL(uint32_t x, uint8_t n)
